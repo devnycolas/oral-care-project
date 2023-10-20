9e1c94cddfa83012367647a702f7105b642b194e
FBC-LTS <fabricioalcc@gmail.com>
2023-10-20 14:06:27 -0300

    Update Participantes.py

--- paginas/Participantes.py
+++ paginas/Participantes.py
@@ -7,9 +7,13 @@ def participantes(df):
     st.write("Graficos Voltados para quem respondeu a pesquisa")
     col1, col2 = st.columns(2)
     col3 = st.columns(1)[0]
+    idade = st.columns([2, 1])
+    escova = st.columns([2, 1])
     plot_pizza_genero(df, col1)
     plot_habito_fumar(df, col2)
     plot_skin_care_genero(df, col3)
+    plot_idade_pesquisados(df, idade)
+    plot_troca_escova_pesquisados(df, escova)
 
 
 def plot_pizza_genero(df: pd.DataFrame, contx: st):
@@ -28,19 +32,51 @@ def plot_habito_fumar(df: pd.DataFrame, contx: st):
 
 
 def plot_skin_care_genero(df: pd.DataFrame, contx: st):
-    routine_skin_care_counts = df.groupby(['genero', 'rotina_skin_care']).size().unstack().fillna(0)
+    df_sc = df.groupby(['genero', 'rotina_skin_care']).size().unstack().fillna(0)
     # Reorganize os dados para criar o gráfico
-    routine_skin_care_counts = routine_skin_care_counts.reset_index()
-    routine_skin_care_counts = pd.melt(routine_skin_care_counts, id_vars='genero', var_name='Rotina de Skin Care', value_name='Contagem')
-    routine_skin_care_counts = routine_skin_care_counts.sort_values(by='Contagem', ascending=False)
+    df_sc = df_sc.reset_index()
+    df_sc = pd.melt(df_sc, id_vars='genero', var_name='Rotina de Skin Care', value_name='Contagem')
+    df_sc = df_sc.sort_values(by='Contagem', ascending=False)
     colors = {'Sim': '#782f40', 'Não': '#00274c'}
     # Gráfico de Barras para Rotina de Skin Care por Gênero
-    fig_bar = px.bar(routine_skin_care_counts, x='genero', y='Contagem', title="Rotina de Skin Care por Gênero",
+    fig = px.bar(df_sc, x='genero', y='Contagem', title="Rotina de Skin Care por Gênero",
                     color='Rotina de Skin Care', color_discrete_map=colors, barmode='group')
     # Personalize as cores, se desejar
-    fig_bar.update_traces(marker_line_color='black', marker_line_width=1)
+    fig.update_traces(marker_line_color='black', marker_line_width=1)
     # Personalize os rótulos dos eixos, se necessário
-    fig_bar.update_xaxes(title='Gênero')
-    fig_bar.update_yaxes(title='Quantidade')
+    fig.update_xaxes(title='Gênero')
+    fig.update_yaxes(title='Quantidade')
     # Exiba o gráfico
-    contx.plotly_chart(fig_bar, use_container_width=True)
+    contx.plotly_chart(fig, use_container_width=True)
+    
+def plot_idade_pesquisados(df: pd.DataFrame, contx: list):
+    # Crie um histograma das idades
+    fig = px.histogram(df, x='idade', title="Distribuição de Idades dos Respondentes")
+    fig.update_xaxes(title_text='Idade')
+    fig.update_yaxes(title_text='Contagem')
+    fig.update_traces(marker_line_color='black', marker_line_width=1)
+    fig.update_traces(marker_color='#782f40')
+    contx[0].plotly_chart(fig)
+    media_idade = int(df['idade'].mean())
+
+    # Exiba um st.metric com a média de idade
+    contx[1].metric("Média de ", f'{media_idade} Anos')
+
+
+def plot_troca_escova_pesquisados(df: pd.DataFrame, contx: st):
+    ordem = ['Todo mês', 'A cada 3 meses', 'A cada 6 meses', '1 vez por ano ou mais']
+    # Converter a coluna 'substitui_escova_dentes' em uma variável categórica
+    df['substitui_escova_dentes'] = df['substitui_escova_dentes'].astype('category')
+    agrupado = df.groupby('substitui_escova_dentes').size().reset_index(name='Contagem')
+    fig = px.bar(agrupado, x='substitui_escova_dentes', y='Contagem', title="Substituição escova", barmode='group')
+    
+    fig.update_xaxes(title_text='')
+    fig.update_yaxes(title_text='Contagem')
+    fig.update_traces(marker_line_color='black', marker_line_width=1)
+    fig.update_traces(marker_color='#782f40')
+    fig.update_xaxes(categoryarray=ordem)
+
+    contx[0].plotly_chart(fig)
+    media = int(df['frequencia_escova_dentes_diaria'].mean())
+    contx[1].metric("Média de escovação diaria", f'{media} vezes')
+
