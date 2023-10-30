9b920557bff30ce4e352c068fbbaf925a6cd6bda
FBC-LTS <fabricioalcc@gmail.com>
2023-10-30 09:06:06 -0300

    Update Sobre.py

--- paginas/Sobre.py
+++ paginas/Sobre.py
@@ -3,14 +3,15 @@ import plotly.express as px
 import pandas as pd
 from Modules.dados import pegar_dados
 
-def sobre(df):
+def sobre(df: pd.DataFrame):
     st.title("Sobre a Equipe e o Projeto")
 
     # Seção do projeto de pesquisa de dados
     st.header("Projeto de Pesquisa de Dados")
 
     # Descrição do projeto
-    st.markdown("Em parceria com uma empresa de odontologia, realizamos um projeto de pesquisa de dados focado em informações relacionadas à saúde bucal.")
+    st.markdown("Em parceria com uma equipe de odontologia, realizamos um projeto de pesquisa de dados focado em informações relacionadas aos cuidados bucais.")
+    st.markdown("Nosso projeto permite integração em tempo real com o formulário de pesquisa, possibilitando a atualização dos valores caso um novo respondente preencha a pesquisa.")
 
     # Objetivos do projeto
     st.subheader("Objetivos do Projeto:")
@@ -18,21 +19,24 @@ def sobre(df):
     st.markdown("- Realizar análises de dados para identificar tendências e insights valiosos.")
     st.markdown("- Colaborar com a empresa de odontologia para melhorar a conscientização e os serviços de saúde bucal.")
     # Criar uma estrutura de colunas para colocar as imagens lado a lado
-    col1, col2 = st.columns(2)
+    col_img_1, col_img_2 = st.columns(2)
 
-    # Membro 1
-    '''
-    with col1:
-        st.image("Modules/img/TI.jpg", width=300, caption="Equipe de TI")
-        st.write("Cargo/Descrição: montar uma pesquisa e fazer o tratamento dos dados impostos pelo Cliente. (Odontologia)")
+    
+    col_img_1.image("Modules/img/odonto.jpg", width=300, caption="Equipe de Odonto")
+    col_img_1.write("Equipe de odontologia que precisa de uma pesquisa e analise para apresentar os dados sobre Oral Care.")
+    
+    
+    col_img_2.image("Modules/img/TI.jpg", width=300, caption="Equipe de TI")
+    col_img_2.write("Equipe de TI com o objetivo de construir a pesquisa e a dash board para equipe de odontologia.")
 
-    # Membro 2
-    with col2:
-        st.image("Modules/img/odonto.jpg", width=300, caption="Equipe de Odonto")
-        st.write("Equipe que montou as perguntas e vai usar os dados da melhor maneira para ajudar seus clientes no dia a dia e futuros clientes.")
-    '''
+    st.divider()
     # Exibir resultados do projeto, como um gráfico
-    st.subheader("Resultados do Projeto")
+    col1, col2 = st.columns(2)
+    col1.subheader("Resultados do Projeto")
+
+    participantes = int(df["genero"].count())
+    col2.metric("Total de ", f'{participantes} participantes')
+
 
     # Criar um gráfico de barras para contar o número de pessoas por gênero com cores diferentes
     df_counts = df.groupby("genero").size().reset_index(name="Número de Pessoas")
@@ -43,9 +47,8 @@ def sobre(df):
     
     # Personalize o rótulo do eixo y
     fig_bar.update_yaxes(title="Número de Pessoas")
-    
 
     
     # Exiba o gráfico
     st.plotly_chart(fig_bar, use_container_width=True)
-
+    
